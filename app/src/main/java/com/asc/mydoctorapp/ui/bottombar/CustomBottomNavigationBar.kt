8861848c21187920b0ac7804065eaac07f830481
Artem Sivokoz <asi@aiweapps.com>
2025-07-20 18:13:30 +0300

    improve bottom navigation bar clicks

--- app/src/main/java/com/asc/mydoctorapp/ui/bottombar/CustomBottomNavigationBar.kt
+++ app/src/main/java/com/asc/mydoctorapp/ui/bottombar/CustomBottomNavigationBar.kt
@@ -2,6 +2,7 @@ package com.asc.mydoctorapp.ui.bottombar
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,10 +12,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -66,7 +67,12 @@ private fun BottomBarItemComponent(
 ) {
     Column(
         modifier = Modifier
-            .clickable(onClick = onItemClick)
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
+                onItemClick()
+            }
             .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
